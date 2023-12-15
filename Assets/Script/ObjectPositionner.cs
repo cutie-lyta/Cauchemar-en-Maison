9282e1b9e950727a19089d365798636985dfd5a5
Atylla <cutie.lyta@gmail.com>
2023-12-15 15:05:39 +0100

    Clock + Poof

--- Assets/Script/ObjectPositionner.cs
+++ Assets/Script/ObjectPositionner.cs
@@ -6,14 +6,21 @@ public class ObjectPositionner : MonoBehaviour
 {
     [SerializeField] private RaycastBehaviour _raycast;
     [SerializeField] private List<Objet> _objets;
-
+    [SerializeField] private GameObject _poofPrefabs;
+    [SerializeField] private GameObject _pffPrefabs;
+    
     private void ReplaceObject(GameObject gameObject)
     {
         foreach (Objet objet in _objets)
         {
             if(objet.pointer == gameObject)
             {
+                GameObject pff = Instantiate(_pffPrefabs);
+                pff.transform.position = gameObject.transform.position;
+                
                 gameObject.transform.position = objet.transform.position;
+                GameObject poof = Instantiate(_poofPrefabs);
+                poof.transform.position = objet.transform.position;
                 break;
             }
         }
