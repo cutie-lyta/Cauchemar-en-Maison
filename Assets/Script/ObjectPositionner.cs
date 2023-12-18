840d386d297edef476039569c4aa5f0557296700
Yoann | Finiarael <yoann.lemetayer@e-artsup.net>
2023-12-18 09:34:22 +0100

    Build 1 (ajout du menu + ecran de fin + retrait du object creator

--- Assets/Script/ObjectPositionner.cs
+++ Assets/Script/ObjectPositionner.cs
@@ -8,22 +8,34 @@ public class ObjectPositionner : MonoBehaviour
     [SerializeField] private List<Objet> _objets;
     [SerializeField] private GameObject _poofPrefabs;
     [SerializeField] private GameObject _pffPrefabs;
-    
+
+    [SerializeField] private SceneLoader _loader;
+
     private void ReplaceObject(GameObject gameObject)
     {
+        Objet searchedObjet = new Objet();
+
         foreach (Objet objet in _objets)
         {
             if(objet.pointer == gameObject)
             {
+
                 GameObject pff = Instantiate(_pffPrefabs);
                 pff.transform.position = gameObject.transform.position;
-                
+
                 gameObject.transform.position = objet.transform.position;
+
                 GameObject poof = Instantiate(_poofPrefabs);
                 poof.transform.position = objet.transform.position;
+
+                searchedObjet = objet;
                 break;
             }
         }
+
+        _objets.Remove(searchedObjet);
+
+        CheckFinish();
     }
     // Start is called before the first frame update
     void Start()
@@ -35,5 +47,14 @@ public class ObjectPositionner : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void CheckFinish()
+    {
+        Debug.Log("Check");
+        if (_objets.Count < 1)
+        {
+            _loader.LoadTheScene("TheEnd");
+        }
     }
 }
