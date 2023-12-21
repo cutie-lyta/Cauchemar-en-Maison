e08373076fa3e037e480f479d026e1d3a5cccfea
Yoann | Finiarael <yoann.lemetayer@e-artsup.net>
2023-12-21 11:32:47 +0100

    Animation du panel et du loading screen mais dum dum brain a restore sa scene du coup bah a pu le panel

---
+++ Assets/Script/UI/LoadingScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingScript : MonoBehaviour
+{
+    [SerializeField] private ClockBehaviour _clockBehaviour;
+    [SerializeField] private float _loadingTime;
+
+    private Animator _anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    private IEnumerator Loading()
+    {
+        yield return new WaitForSeconds(_loadingTime);
+
+        _anim.SetTrigger("FadeOut");
+
+        yield return null;
+    }
+
+    //Called by the Animator when the fading animation is finished
+    private void FadeFinished()
+    {
+        _clockBehaviour.TimerStart();
+    }    
+}
