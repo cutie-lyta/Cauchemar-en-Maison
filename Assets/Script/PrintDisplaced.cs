22aeb1324bbbe8c873d4b373d77e2224a128c295
elagmae <lea.pouthier@gmail.com>
2023-12-21 10:09:19 +0100

    compteur d'objets

--- Assets/Script/PrintDisplaced.cs
+++ Assets/Script/PrintDisplaced.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PrintDisplaced : MonoBehaviour
@@ -7,31 +8,23 @@ public class PrintDisplaced : MonoBehaviour
     
 {
     [SerializeField] private ObjectPositionner _objectPositionner;
-    private int _nbDisplaced;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        var textMesh = GetComponent<TextMesh>();
-
-        _nbDisplaced = _objectPositionner.Objets.Count;
-        string _txtDisplaced = _nbDisplaced.ToString();        
-        textMesh.text = _txtDisplaced ;
-    }
 
     // Update is called once per frame
     void Update()
     {
-        var textMesh = GetComponent<TextMesh>();
+        int _nbDisplaced = _objectPositionner.Objets.Count;
+        var textMesh = GetComponent<TextMeshProUGUI>();
 
         foreach (Objet obj in _objectPositionner.Objets)
         {
-            if (obj.pointer.CompareTag("Displaced"))
+            if (!obj.pointer.CompareTag("Displaced"))
             {
                 _nbDisplaced--;
-                string _txtDisplaced = _nbDisplaced.ToString();
-                textMesh.text = _txtDisplaced ;
+                
             }
         }
+
+        string _txtDisplaced = _nbDisplaced.ToString();
+        textMesh.text = _txtDisplaced;
     }
 }
